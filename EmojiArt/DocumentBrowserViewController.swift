44ca79f59a822b3ac5a4d040a68eb5a583785118
Pavel <pavel.m.3107@gmail.com>
2020-04-29 17:00:22 +0300

    Lessons #14

--- EmojiArt/DocumentBrowserViewController.swift
+++ EmojiArt/DocumentBrowserViewController.swift
@@ -13,37 +13,41 @@ class DocumentBrowserViewController: UIDocumentBrowserViewController, UIDocument
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         delegate = self
-        
-        allowsDocumentCreation = true
         allowsPickingMultipleItems = false
+        allowsDocumentCreation = false
         
-        // Update the style of the UIDocumentBrowserViewController
-        // browserUserInterfaceStyle = .dark
-        // view.tintColor = .white
-        
-        // Specify the allowed content types of your application via the Info.plist.
-        
-        // Do any additional setup after loading the view.
-    }
-    
-    
-    // MARK: UIDocumentBrowserViewControllerDelegate
-    
-    func documentBrowser(_ controller: UIDocumentBrowserViewController, didRequestDocumentCreationWithHandler importHandler: @escaping (URL?, UIDocumentBrowserViewController.ImportMode) -> Void) {
-        let newDocumentURL: URL? = nil
-        
-        // Set the URL for the new document here. Optionally, you can present a template chooser before calling the importHandler.
-        // Make sure the importHandler is always called, even if the user cancels the creation request.
-        if newDocumentURL != nil {
-            importHandler(newDocumentURL, .move)
-        } else {
-            importHandler(nil, .none)
+        if UIDevice.current.userInterfaceIdiom == .pad {
+            
+            template = try? FileManager.default.url(
+                for: .applicationSupportDirectory,
+                in: .userDomainMask,
+                appropriateFor: nil,
+                create: true
+                ).appendingPathComponent("Untitled.json")
+            if template != nil {
+                allowsDocumentCreation =
+                    FileManager.default.createFile(atPath: template!.path,
+                                                 contents: Data())
+            }
         }
     }
     
-    func documentBrowser(_ controller: UIDocumentBrowserViewController, didPickDocumentsAt documentURLs: [URL]) {
+     var template: URL? // blank template for new documents
+    
+    // MARK: UIDocumentBrowserViewControllerDelegate
+    
+    func documentBrowser(
+        _ controller: UIDocumentBrowserViewController,
+        didRequestDocumentCreationWithHandler importHandler: @escaping (URL?, UIDocumentBrowserViewController.ImportMode
+        ) -> Void) {
+            importHandler(template, .copy)
+    }
+    
+    func documentBrowser(
+        _ controller: UIDocumentBrowserViewController,
+        didPickDocumentURLs documentURLs: [URL]
+        ) {
         guard let sourceURL = documentURLs.first else { return }
         
         // Present the Document View Controller for the first document that was picked.
@@ -51,25 +55,35 @@ class DocumentBrowserViewController: UIDocumentBrowserViewController, UIDocument
         presentDocument(at: sourceURL)
     }
     
-    func documentBrowser(_ controller: UIDocumentBrowserViewController, didImportDocumentAt sourceURL: URL, toDestinationURL destinationURL: URL) {
+    func documentBrowser(
+        _ controller: UIDocumentBrowserViewController,
+        didImportDocumentAt sourceURL: URL,
+        toDestinationURL destinationURL: URL
+        ) {
         // Present the Document View Controller for the new newly created document
         presentDocument(at: destinationURL)
     }
     
-    func documentBrowser(_ controller: UIDocumentBrowserViewController, failedToImportDocumentAt documentURL: URL, error: Error?) {
+    func documentBrowser(
+        _ controller: UIDocumentBrowserViewController,
+        failedToImportDocumentAt documentURL: URL,
+        error: Error?) {
         // Make sure to handle the failed import appropriately, e.g., by presenting an error message to the user.
     }
     
     // MARK: Document Presentation
     
     func presentDocument(at documentURL: URL) {
-        
         let storyBoard = UIStoryboard(name: "Main", bundle: nil)
-        let documentViewController = storyBoard.instantiateViewController(withIdentifier: "DocumentViewController") as! DocumentViewController
-        documentViewController.document = Document(fileURL: documentURL)
-        documentViewController.modalPresentationStyle = .fullScreen
+        let documentVC =
+            storyBoard.instantiateViewController(withIdentifier: "DocumentMVC")
+        if let emojiArtViewController =
+                              documentVC.contents as? EmojiArtViewController {
+            emojiArtViewController.document = EmojiArtDocument(fileURL: documentURL)
+
+        }
+         present(documentVC, animated: true)
         
-        present(documentViewController, animated: true, completion: nil)
     }
 }
 
---
+++ EmojiArt/EmojiCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  EmojiCollectionViewCell.swift
+//  EmojiArt
+//
+//  Created by Pavel Ronaldo on 4/26/20.
+//  Copyright © 2020 Pavel Ronaldo. All rights reserved.
+//
+
+import UIKit
+
+class EmojiCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var label: UILabel!
+}
